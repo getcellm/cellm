6cd5156b1ed68cd7115be1b6b723e3c1da67b9bc
Kasper Marstal <kaspermarstal@gmail.com>
2024-09-05 22:05:23 +0200

    feat: Add rate limiter and circuit breaker to http clients

--- Cellm/ServiceLocator.cs
+++ Cellm/ServiceLocator.cs
@@ -6,9 +6,11 @@ using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
+using Polly.CircuitBreaker;
 using Polly.Retry;
 using Polly.Timeout;
 using System.Net;
+using System.Threading.RateLimiting;
 
 namespace Cellm;
 
@@ -54,8 +56,8 @@ internal static class ServiceLocator
             anthropicHttpClient.DefaultRequestHeaders.Add("anthropic-version", anthropicConfiguration.Version);
         }).AddResilienceHandler("AnthropicResiliencePipeline", ConfigureResiliencePipeline);
 
-        var googleGeminiConfiguration = configuration.GetRequiredSection(nameof(GoogleGeminiConfiguration)).Get<GoogleGeminiConfiguration>()
-            ?? throw new NullReferenceException(nameof(GoogleGeminiConfiguration));
+        var googleGeminiConfiguration = configuration.GetRequiredSection(nameof(GoogleConfiguration)).Get<GoogleConfiguration>()
+            ?? throw new NullReferenceException(nameof(GoogleConfiguration));
 
         services.AddHttpClient<GoogleClient>(googleGeminiHttpClient =>
         {
@@ -77,32 +79,64 @@ internal static class ServiceLocator
     static void ConfigureResiliencePipeline(ResiliencePipelineBuilder<HttpResponseMessage> builder)
     {
         _ = builder
+            .AddRateLimiter(new TokenBucketRateLimiter(new TokenBucketRateLimiterOptions
+            {
+                TokenLimit = 60,
+                QueueLimit = int.MaxValue,
+                ReplenishmentPeriod = TimeSpan.FromSeconds(1),
+                TokensPerPeriod = 1,
+            }))
+            .AddCircuitBreaker(new CircuitBreakerStrategyOptions<HttpResponseMessage>
+            {
+                ShouldHandle = args => ValueTask.FromResult(ShouldBreakCircuit(args.Outcome)),
+                SamplingDuration = TimeSpan.FromSeconds(30),
+                FailureRatio = 0.5,
+                MinimumThroughput = 10,
+                BreakDuration = TimeSpan.FromSeconds(5),
+            })
             .AddRetry(new RetryStrategyOptions<HttpResponseMessage>
             {
                 ShouldHandle = args => ValueTask.FromResult(ShouldRetry(args.Outcome)),
                 BackoffType = DelayBackoffType.Exponential,
                 UseJitter = true,
-                MaxRetryAttempts = 7,
-                Delay = TimeSpan.FromSeconds(2),
+                MaxRetryAttempts = 8,
+                Delay = TimeSpan.FromSeconds(1),
             })
             // Timeout for a single request
             .AddTimeout(TimeSpan.FromSeconds(10));
     }
 
-
-    private static bool ShouldRetry(Outcome<HttpResponseMessage> outcome) => outcome switch
+    private static bool ShouldBreakCircuit(Outcome<HttpResponseMessage> outcome) => outcome switch
     {
-        { Result: HttpResponseMessage response } => IsServerError(response),
-        { Exception: Exception exception } => IsTransientException(exception),
+        { Result: HttpResponseMessage response } => IsCircuitBreakerError(response),
+        { Exception: Exception exception } => IsCircuitBreakerException(exception),
         _ => false
     };
 
-    private static bool IsServerError(HttpResponseMessage response) =>
+    private static bool IsCircuitBreakerError(HttpResponseMessage response) =>
+        response.StatusCode >= HttpStatusCode.InternalServerError ||
+        response.StatusCode == HttpStatusCode.ServiceUnavailable ||
+        response.StatusCode == HttpStatusCode.TooManyRequests;
+
+    private static bool IsCircuitBreakerException(Exception exception) =>
+        IsRetryableException(exception) || IsCatastrophicException(exception);
+
+    private static bool IsCatastrophicException(Exception exception) =>
+        exception is OutOfMemoryException or ThreadAbortException;
+
+    private static bool ShouldRetry(Outcome<HttpResponseMessage> outcome) => outcome switch
+    {
+        { Result: HttpResponseMessage response } => IsRetryableError(response),
+        { Exception: Exception exception } => IsRetryableException(exception),
+        _ => false
+    };
+
+    private static bool IsRetryableError(HttpResponseMessage response) =>
         response.StatusCode >= HttpStatusCode.InternalServerError ||
         response.StatusCode == HttpStatusCode.RequestTimeout ||
         response.StatusCode == HttpStatusCode.TooManyRequests;
 
-    private static bool IsTransientException(Exception exception) =>
+    private static bool IsRetryableException(Exception exception) =>
         exception is HttpRequestException or TimeoutRejectedException;
 
     public static T Get<T>() where T : notnull
