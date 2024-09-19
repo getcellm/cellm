e49dd72ec8272edf321932535b3f1237208c40e5
Kasper Marstal <kaspermarstal@gmail.com>
2024-09-19 22:56:26 +0200

    feat: Add LlamafileClient

---
+++ src/Cellm/Models/Llamafile/LlamafileConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace Cellm.Models.Llamafile;
+
+internal class LlamafileConfiguration
+{
+    public Uri LlamafileUrl { get; init; }
+
+    public Dictionary<string, Uri> Models { get; init; }
+
+    public string DefaultModel { get; init; }
+
+    public ushort Port { get; init; }
+
+    public bool Gpu { get; init; }
+
+    public int GpuLayers { get; init; }
+
+    public LlamafileConfiguration()
+    {
+        LlamafileUrl = default!;
+        Models = default!;
+        DefaultModel = default!;
+        Gpu = false;
+        GpuLayers = 999;
+    }
+}
