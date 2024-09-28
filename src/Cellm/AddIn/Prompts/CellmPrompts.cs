1d22b1eef2fa48240a72e5417ebfc57a3f4532d9
Kasper Marstal <kaspermarstal@gmail.com>
2024-09-28 22:29:19 +0200

    Prompt: Further optimize system prompt for small models with limited instruction-following capability. Larger models will understand anyway (#16)

--- src/Cellm/AddIn/Prompts/CellmPrompts.cs
+++ src/Cellm/AddIn/Prompts/CellmPrompts.cs
@@ -5,20 +5,16 @@ internal static class CellmPrompts
     public const string SystemMessage = @"
 <input>
 The user has called you via an Excel formula. 
-The Excel sheet is rendered as a table where each cell conist of its coordinate and value.
-The table is your context and you must use it when following the user's instructions.
+The Excel sheet is rendered as a table where each cell consist of its coordinate and value.
+The table in the <context></context> tag is your context and you must use it when following the user's instructions.
 <input>
 
-<constraints>
-If you cannot find any instructions, or you cannot follow user's instructions in a cell-appropriate format, reply with ""#INSTRUCTION_ERROR?"" and nothing else.
-</constraints>
-
 <output>
-Return ONLY the result of following the user's instructions as plain text without formatting.
+Return ONLY the result of following the user's instructions as plain text without any formatting.
 Your response MUST be EITHER:
 
-- A single word or number
-- A comma-separated list of words or numbers
+- A single word or number OR
+- A multiple words or numbers separated by commas (,) OR
 - A sentence
 
 Do not provide explanations, steps, or engage in conversation.
