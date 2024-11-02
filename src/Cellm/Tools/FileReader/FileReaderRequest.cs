df2fcea8622e4e6666d6fd71a18fcf6175af2ebc
Kasper Marstal <kaspermarstal@gmail.com>
2024-11-02 21:36:57 +0100

    fix: Parse tool description attributes (#45)

--- src/Cellm/Tools/FileReader/FileReaderRequest.cs
+++ src/Cellm/Tools/FileReader/FileReaderRequest.cs
@@ -3,4 +3,5 @@ using MediatR;
 
 namespace Cellm.Tools.FileReader;
 
+[Description("Reads the contents of file and returns plain text.")]
 internal record FileReaderRequest([Description("The absolute path to the file.")] string FilePath) : IRequest<FileReaderResponse>;
