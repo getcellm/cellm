d991d838f2742add45eba1b67b8fe0a76be03fdc
Kasper Marstal <kaspermarstal@gmail.com>
2025-01-26 10:07:44 +0100

    bug: Fix OpenAiCompatible API key (#94)

--- src/Cellm/Models/Providers/OpenAiCompatible/OpenAiCompatibleRequestHandler.cs
+++ src/Cellm/Models/Providers/OpenAiCompatible/OpenAiCompatibleRequestHandler.cs
@@ -11,12 +11,10 @@ internal class OpenAiCompatibleRequestHandler(
 
     public async Task<OpenAiCompatibleResponse> Handle(OpenAiCompatibleRequest request, CancellationToken cancellationToken)
     {
-        var api_key = string.IsNullOrEmpty(request.ApiKey) ? "API_KEY" : request.ApiKey;
-
         var chatClient = openAiCompatibleChatClientFactory.Create(
             request.BaseAddress ?? openAiCompatibleConfiguration.Value.BaseAddress,
             request.Prompt.Options.ModelId ?? string.Empty,
-            api_key);
+            openAiCompatibleConfiguration.Value.ApiKey);
 
         var chatCompletion = await chatClient.CompleteAsync(request.Prompt.Messages, request.Prompt.Options, cancellationToken);
 
