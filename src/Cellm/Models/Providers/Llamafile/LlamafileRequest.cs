1b774243dbe48ab2fe77655cedb733888528bcd4
Kasper Marstal <kaspermarstal@gmail.com>
2025-02-05 21:52:40 +0100

    fix: Changing API keys while app is running

--- src/Cellm/Models/Providers/Llamafile/LlamafileRequest.cs
+++
@@ -1,5 +0,0 @@
-﻿using Cellm.Models.Prompts;
-
-namespace Cellm.Models.Providers.Llamafile;
-
-internal record LlamafileRequest(Prompt Prompt) : IProviderRequest<LlamafileResponse>;
--- src/Cellm/Models/Providers/Llamafile/LlamafileResponse.cs
+++
@@ -1,5 +0,0 @@
-﻿using Cellm.Models.Prompts;
-
-namespace Cellm.Models.Providers.Llamafile;
-
-internal record LlamafileResponse(Prompt Prompt) : IModelResponse;
