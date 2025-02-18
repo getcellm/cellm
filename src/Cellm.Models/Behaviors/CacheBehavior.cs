06b6c8282cf264ef9904d5fe15b19dfff558a5f0
Kasper Marstal <kaspermarstal@gmail.com>
2025-02-18 11:19:34 +0100

    fix: Prompt not cached because cache key length sometimes too long

--- src/Cellm.Models/Behaviors/CacheBehavior.cs
+++ src/Cellm.Models/Behaviors/CacheBehavior.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 using Cellm.Models.Providers;
 using MediatR;
 using Microsoft.Extensions.Caching.Hybrid;
@@ -15,14 +17,17 @@ internal class CacheBehavior<TRequest, TResponse>(HybridCache cache, IOptionsMon
         Expiration = TimeSpan.FromSeconds(providerConfiguration.CurrentValue.CacheTimeoutInSeconds)
     };
 
-    private static List<string> Tags = [nameof(IModelResponse)];
+    private static readonly List<string> Tags = [nameof(IModelResponse)];
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         if (providerConfiguration.CurrentValue.EnableCache)
         {
+            byte[] hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(request.Prompt)));
+            var key = Convert.ToBase64String(hashBytes);
+
             return await cache.GetOrCreateAsync(
-                JsonSerializer.Serialize(request.Prompt),
+                key,
                 async cancel => await next(),
                 options: _cacheEntryOptions,
                 Tags,
