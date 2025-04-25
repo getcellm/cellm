7b3b83c914a977df956340c6fcafef9b158b34e6
Kasper Marstal <kaspermarstal@gmail.com>
2025-04-25 22:59:42 +0200

    refactor: Provider settings form layout

--- src/Cellm/AddIn/UserInterface/Forms/ProviderSettingsForm.Designer.cs
+++ src/Cellm/AddIn/UserInterface/Forms/ProviderSettingsForm.Designer.cs
@@ -20,98 +20,102 @@ partial class ProviderSettingsForm
     /// </summary>
     private void InitializeComponent()
     {
-        this.apiKeyLabel = new System.Windows.Forms.Label();
-        this.apiKeyTextBox = new System.Windows.Forms.TextBox();
-        this.baseAddressLabel = new System.Windows.Forms.Label();
-        this.baseAddressTextBox = new System.Windows.Forms.TextBox();
-        this.okButton = new System.Windows.Forms.Button();
-        this.cancelButton = new System.Windows.Forms.Button();
-        this.SuspendLayout();
-        //
+        apiKeyLabel = new Label();
+        apiKeyTextBox = new TextBox();
+        baseAddressLabel = new Label();
+        baseAddressTextBox = new TextBox();
+        okButton = new Button();
+        cancelButton = new Button();
+        SuspendLayout();
+        // 
         // apiKeyLabel
-        //
-        this.apiKeyLabel.AutoSize = true;
-        this.apiKeyLabel.Location = new System.Drawing.Point(12, 15);
-        this.apiKeyLabel.Name = "apiKeyLabel";
-        this.apiKeyLabel.Size = new System.Drawing.Size(57, 13);
-        this.apiKeyLabel.TabIndex = 0;
-        this.apiKeyLabel.Text = "API Key:";
-        //
+        // 
+        apiKeyLabel.AutoSize = true;
+        apiKeyLabel.Location = new Point(14, 17);
+        apiKeyLabel.Margin = new Padding(4, 0, 4, 0);
+        apiKeyLabel.Name = "apiKeyLabel";
+        apiKeyLabel.Size = new Size(50, 15);
+        apiKeyLabel.TabIndex = 0;
+        apiKeyLabel.Text = "API Key:";
+        // 
         // apiKeyTextBox
-        //
-        this.apiKeyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.apiKeyTextBox.Location = new System.Drawing.Point(15, 31);
-        this.apiKeyTextBox.Name = "apiKeyTextBox";
-        this.apiKeyTextBox.Size = new System.Drawing.Size(357, 20);
-        this.apiKeyTextBox.TabIndex = 0; // TabIndex 0
-        this.apiKeyTextBox.UseSystemPasswordChar = true;
-        //
+        // 
+        apiKeyTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        apiKeyTextBox.Location = new Point(101, 15);
+        apiKeyTextBox.Margin = new Padding(4, 3, 4, 3);
+        apiKeyTextBox.Name = "apiKeyTextBox";
+        apiKeyTextBox.Size = new Size(218, 23);
+        apiKeyTextBox.TabIndex = 0;
+        apiKeyTextBox.UseSystemPasswordChar = true;
+        // 
         // baseAddressLabel
-        //
-        this.baseAddressLabel.AutoSize = true;
-        this.baseAddressLabel.Location = new System.Drawing.Point(12, 65); // Position below ApiKey
-        this.baseAddressLabel.Name = "baseAddressLabel";
-        this.baseAddressLabel.Size = new System.Drawing.Size(80, 13);
-        this.baseAddressLabel.TabIndex = 2;
-        this.baseAddressLabel.Text = "Base Address:";
-        //
+        // 
+        baseAddressLabel.AutoSize = true;
+        baseAddressLabel.Location = new Point(14, 47);
+        baseAddressLabel.Margin = new Padding(4, 0, 4, 0);
+        baseAddressLabel.Name = "baseAddressLabel";
+        baseAddressLabel.Size = new Size(79, 15);
+        baseAddressLabel.TabIndex = 2;
+        baseAddressLabel.Text = "Base Address:";
+        // 
         // baseAddressTextBox
-        //
-        this.baseAddressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.baseAddressTextBox.Location = new System.Drawing.Point(15, 81); // Position below Base Address Label
-        this.baseAddressTextBox.Name = "baseAddressTextBox";
-        this.baseAddressTextBox.Size = new System.Drawing.Size(357, 20);
-        this.baseAddressTextBox.TabIndex = 1; // TabIndex 1
-        //
+        // 
+        baseAddressTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        baseAddressTextBox.Location = new Point(101, 44);
+        baseAddressTextBox.Margin = new Padding(4, 3, 4, 3);
+        baseAddressTextBox.Name = "baseAddressTextBox";
+        baseAddressTextBox.Size = new Size(218, 23);
+        baseAddressTextBox.TabIndex = 1;
+        // 
         // okButton
-        //
-        this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-        this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-        this.okButton.Location = new System.Drawing.Point(216, 117); // Position bottom right
-        this.okButton.Name = "okButton";
-        this.okButton.Size = new System.Drawing.Size(75, 23);
-        this.okButton.TabIndex = 2; // TabIndex 2
-        this.okButton.Text = "OK";
-        this.okButton.UseVisualStyleBackColor = true;
-        this.okButton.Click += new System.EventHandler(this.okButton_Click);
-        //
+        // 
+        okButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+        okButton.DialogResult = DialogResult.OK;
+        okButton.Location = new Point(137, 84);
+        okButton.Margin = new Padding(4, 3, 4, 3);
+        okButton.Name = "okButton";
+        okButton.Size = new Size(88, 27);
+        okButton.TabIndex = 2;
+        okButton.Text = "OK";
+        okButton.UseVisualStyleBackColor = true;
+        okButton.Click += okButton_Click;
+        // 
         // cancelButton
-        //
-        this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-        this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-        this.cancelButton.Location = new System.Drawing.Point(297, 117); // Position bottom right
-        this.cancelButton.Name = "cancelButton";
-        this.cancelButton.Size = new System.Drawing.Size(75, 23);
-        this.cancelButton.TabIndex = 3; // TabIndex 3
-        this.cancelButton.Text = "Cancel";
-        this.cancelButton.UseVisualStyleBackColor = true;
-        this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
-        //
+        // 
+        cancelButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+        cancelButton.DialogResult = DialogResult.Cancel;
+        cancelButton.Location = new Point(231, 84);
+        cancelButton.Margin = new Padding(4, 3, 4, 3);
+        cancelButton.Name = "cancelButton";
+        cancelButton.Size = new Size(88, 27);
+        cancelButton.TabIndex = 3;
+        cancelButton.Text = "Cancel";
+        cancelButton.UseVisualStyleBackColor = true;
+        cancelButton.Click += cancelButton_Click;
+        // 
         // ProviderSettingsForm
-        //
-        this.AcceptButton = this.okButton;
-        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.CancelButton = this.cancelButton;
-        this.ClientSize = new System.Drawing.Size(384, 152); // Adjust size as needed
-        this.Controls.Add(this.cancelButton);
-        this.Controls.Add(this.okButton);
-        this.Controls.Add(this.baseAddressTextBox);
-        this.Controls.Add(this.baseAddressLabel);
-        this.Controls.Add(this.apiKeyTextBox);
-        this.Controls.Add(this.apiKeyLabel);
-        this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-        this.MaximizeBox = false;
-        this.MinimizeBox = false;
-        this.Name = "ProviderSettingsForm";
-        this.ShowInTaskbar = false;
-        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-        this.Text = "Provider Settings"; // Will be overridden in constructor
-        this.ResumeLayout(false);
-        this.PerformLayout();
-
+        // 
+        AcceptButton = okButton;
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        CancelButton = cancelButton;
+        ClientSize = new Size(333, 124);
+        Controls.Add(cancelButton);
+        Controls.Add(okButton);
+        Controls.Add(baseAddressTextBox);
+        Controls.Add(baseAddressLabel);
+        Controls.Add(apiKeyTextBox);
+        Controls.Add(apiKeyLabel);
+        FormBorderStyle = FormBorderStyle.FixedDialog;
+        Margin = new Padding(4, 3, 4, 3);
+        MaximizeBox = false;
+        MinimizeBox = false;
+        Name = "ProviderSettingsForm";
+        ShowInTaskbar = false;
+        StartPosition = FormStartPosition.CenterParent;
+        Text = "Provider Settings";
+        ResumeLayout(false);
+        PerformLayout();
     }
     #endregion
 }
