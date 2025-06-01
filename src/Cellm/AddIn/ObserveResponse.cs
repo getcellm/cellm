bca502ee349493ea9e004484f07c8a29c1d317c4
Kasper Marstal <kaspermarstal@gmail.com>
2025-06-02 01:15:35 +0200

    feat: Fully unblock Excel's main thread when starting many prompts (#183)

--- src/Cellm/AddIn/ObserveResponse.cs
+++ src/Cellm/AddIn/ObserveResponse.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
+using Cellm.AddIn.Exceptions;
 using Cellm.Models;
 using Cellm.Models.Prompts;
 using ExcelDna.Integration;
@@ -8,12 +10,10 @@ using Microsoft.Extensions.Options;
 
 namespace Cellm.AddIn;
 
-internal class ObserveResponse(Arguments arguments) : IExcelObservable
+internal class ObserveResponse(Arguments arguments, Stopwatch stopwatch) : IExcelObservable
 {
-    private IExcelObserver? _observer;
     private Task? _task;
     private readonly CancellationTokenSource _cancellationTokenSource = new();
-    private readonly Lock _lock = new();
 
     private readonly ILogger _logger = CellmAddIn.Services
         .GetRequiredService<ILoggerFactory>()
@@ -21,98 +21,92 @@ internal class ObserveResponse(Arguments arguments) : IExcelObservable
 
     public IDisposable Subscribe(IExcelObserver observer)
     {
-        lock (_lock)
-        {
-            if (_observer is not null)
-            {
-                throw new InvalidOperationException("Internal error: GetResponse instance has already been subscribed to. Each call requires a new instance.");
-            }
+        var elapsedSubscribeStart = stopwatch.ElapsedMilliseconds;
 
-            _logger.LogDebug("Getting response ...");
-            _observer = observer ?? throw new ArgumentNullException(nameof(observer));
-        }
+        _task = Task.Run(() => GetResponse(observer));
 
-        _observer.OnNext(ExcelError.ExcelErrorGettingData);
-
-        _task = Task.Run(async () =>
-        {
-            try
-            {
-                var cells = arguments.Cells switch
-                {
-                    Cells argumentCells => ArgumentParser.ParseCells(argumentCells),
-                    null => "Not available",
-                    _ => throw new ArgumentException(nameof(arguments.Cells))
-                };
-
-                var instructions = arguments.Instructions switch
-                {
-                    string instruction => instruction,
-                    Cells values => ArgumentParser.ParseCells(values),
-                    _ => throw new ArgumentException(nameof(arguments.Instructions))
-                };
-
-                var userMessage = new StringBuilder()
-                    .AppendLine(ArgumentParser.AddInstructionTags(instructions))
-                    .AppendLine(ArgumentParser.AddCellTags(cells))
-                    .ToString();
-
-                var cellmAddInConfiguration = CellmAddIn.Services.GetRequiredService<IOptionsMonitor<CellmAddInConfiguration>>();
-
-                var prompt = new PromptBuilder()
-                    .SetModel(arguments.Model)
-                    .SetTemperature(arguments.Temperature)
-                    .SetMaxOutputTokens(cellmAddInConfiguration.CurrentValue.MaxOutputTokens)
-                    .AddSystemMessage(SystemMessages.SystemMessage)
-                    .AddUserMessage(userMessage)
-                    .Build();
-
-                // Check for cancellation before sending request
-                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
-
-                var client = CellmAddIn.Services.GetRequiredService<Client>();
-                var response = await client.GetResponseAsync(prompt, arguments.Provider, _cancellationTokenSource.Token);
-                var assistantMessage = response.Messages.LastOrDefault()?.Text ?? throw new InvalidOperationException("No text response");
-
-                // Check for cancellation before notifying observer
-                _cancellationTokenSource.Token.ThrowIfCancellationRequested();
-
-                // Notify observer on the main thread
-                ExcelAsyncUtil.QueueAsMacro(() =>
-                {
-                    _observer?.OnNext(assistantMessage);
-                    _observer?.OnCompleted();
-                });
-
-                _logger.LogDebug("Getting response {id} ... Done", _task?.Id);
-            }
-            catch (OperationCanceledException ex)
-            {
-                // Notify observer on the main thread
-                ExcelAsyncUtil.QueueAsMacro(() =>
-                {
-                    _observer.OnError(ex);
-                });
-
-                _logger.LogDebug(ex, "Getting response {id} ... Cancelled", _task?.Id);
-            }
-            catch (Exception ex)
-            {
-                // Notify observer on the main thread
-                ExcelAsyncUtil.QueueAsMacro(() =>
-                {
-                    observer.OnError(ex);
-                });
-
-                _logger.LogError(ex, "Getting response {id} ... Failed: {message}", _task?.Id, ex.Message);
-            }
-        }, _cancellationTokenSource.Token);
+        _logger.LogInformation("Sending request {id} ... Queued (elapsed time: {} ms, queue time: {})", _task?.Id, stopwatch.ElapsedMilliseconds, stopwatch.ElapsedMilliseconds - elapsedSubscribeStart);
 
         return new ActionDisposable(() =>
         {
-            _logger.LogDebug("Getting response {id} ... Disposing ({status})", _task.Id, _task.IsCompleted ? "done" : "cancelled");
             _cancellationTokenSource.Cancel();
         });
+    }
+
+    public async void GetResponse(IExcelObserver observer)
+    {
+        try
+        {
+            var elapsedTaskStart = stopwatch.ElapsedMilliseconds;
+
+            // Check for cancellation before doing any work
+            _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+
+            observer.OnNext(ExcelError.ExcelErrorGettingData);
+
+            var cells = arguments.Cells switch
+            {
+                Cells argumentCells => ArgumentParser.ParseCells(argumentCells),
+                null => "Not available",
+                _ => throw new ArgumentException(nameof(arguments.Cells))
+            };
+
+            var instructions = arguments.Instructions switch
+            {
+                string argumentInstruction => argumentInstruction,
+                Cells argumentInstruction => ArgumentParser.ParseCells(argumentInstruction),
+                _ => throw new ArgumentException(nameof(arguments.Instructions))
+            };
+
+            // Check for cancellation before constructing prompt
+            _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+
+            var userMessage = new StringBuilder()
+                .AppendLine(ArgumentParser.AddInstructionTags(instructions))
+                .AppendLine(ArgumentParser.AddCellTags(cells))
+                .ToString();
+
+            var cellmAddInConfiguration = CellmAddIn.Services.GetRequiredService<IOptionsMonitor<CellmAddInConfiguration>>();
+
+            var prompt = new PromptBuilder()
+                .SetModel(arguments.Model)
+                .SetTemperature(arguments.Temperature)
+                .SetMaxOutputTokens(cellmAddInConfiguration.CurrentValue.MaxOutputTokens)
+                .AddSystemMessage(SystemMessages.SystemMessage)
+                .AddUserMessage(userMessage)
+                .Build();
+
+            // Check for cancellation before sending request
+            _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+
+            var client = CellmAddIn.Services.GetRequiredService<Client>();
+            var response = await client.GetResponseAsync(prompt, arguments.Provider, _cancellationTokenSource.Token).ConfigureAwait(false);
+            var assistantMessage = response.Messages.LastOrDefault()?.Text ?? throw new InvalidOperationException("No text response");
+
+            // Check for cancellation before notifying observer
+            _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+
+            observer.OnNext(assistantMessage);
+            observer.OnCompleted();
+
+            _logger.LogInformation("Sending request {id} ... Done (elapsed time: {} ms, task time: {})", _task?.Id, stopwatch.ElapsedMilliseconds, stopwatch.ElapsedMilliseconds - elapsedTaskStart);
+        }
+        catch (ExcelErrorException ex)
+        {
+            observer.OnNext(ex.GetExcelError());
+            observer.OnCompleted();
+
+        }
+        catch (OperationCanceledException)
+        {
+            // Do not notify observer, it is being disposed or something went wrong
+            _logger.LogInformation("Sending request {id} ... Cancelled (elapsed time: {} ms)", _task?.Id, stopwatch.ElapsedMilliseconds);
+        }
+        catch (Exception ex)
+        {
+            observer.OnError(ex);
+            _logger.LogError(ex, "Sending request {id} ... Failed: {message} (elapsed time: {} ms)", _task?.Id, ex.Message, stopwatch.ElapsedMilliseconds);
+        }
     }
 
     class ActionDisposable(Action disposeAction) : IDisposable
