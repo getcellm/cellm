82096826c167373e7afd0b823c8554c4e88cc668
Kasper Marstal <kaspermarstal@gmail.com>
2025-06-16 22:15:43 +0200

    docs: Add license notice to account configuration (#201)

--- src/Cellm/Users/AccountConfiguration.cs
+++ src/Cellm/Users/AccountConfiguration.cs
@@ -1,4 +1,28 @@
-﻿namespace Cellm.Users;
+﻿// Circumventing limitations anywhere in the codebase is a direct violation of the
+// Fair Core License, Version 1.0 until a commit transitions to the Apache 2.0 Future License.
+// Once a commit transitions to the Apache 2.0 Future License, you can checkout out
+// that commit and use the code under the Apache 2.0 License. A commit transitions to
+// the Apache 2.0 Future License on the second anniversary of the date the git commit
+// became available. 
+//
+// The relevant section of the Fair Core License, Version 1.0 is:
+//
+// > ### Limitations
+// > You must not move, change, disable, or circumvent the license key functionality
+// > in the Software; or modify any portion of the Software protected by the license
+// > key to:
+//
+// > 1. enable access to the protected functionality without a valid license key; or
+//
+// > 2. remove the protected functionality.
+//
+// You can checkout the latest commit licensed under the Apache 2.0 License like this:
+// 
+// $ git checkout $(git rev-list -n 1 --before="2 years ago" HEAD)
+//
+// For more details, go to https://github.com/getcellm/cellm/blob/main/LICENSE.
+
+namespace Cellm.Users;
 
 internal class AccountConfiguration
 {
