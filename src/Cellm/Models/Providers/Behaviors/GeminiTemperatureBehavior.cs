e59bb66610f4229290ce5d0594b469edb228be4e
Kasper Marstal <kaspermarstal@gmail.com>
2025-07-16 15:06:48 +0200

    fix: Revert Gemini temperature range on response

--- src/Cellm/Models/Providers/Behaviors/GeminiTemperatureBehavior.cs
+++ src/Cellm/Models/Providers/Behaviors/GeminiTemperatureBehavior.cs
@@ -1,5 +1,4 @@
 ﻿using Cellm.AddIn;
-using Cellm.Models.Behaviors;
 using Cellm.Models.Prompts;
 using Microsoft.Extensions.Options;
 
@@ -7,6 +6,7 @@ namespace Cellm.Models.Providers.Behaviors;
 
 internal class GeminiTemperatureBehavior(IOptionsMonitor<CellmAddInConfiguration> cellmAddinConfiguration) : IProviderBehavior
 {
+    private const float DefaultMinTemp = 0.0f;
     private const float DefaultMaxTemp = 1.0f;
     private const float GeminiMaxTemperature = 2.0f;
 
@@ -21,11 +21,20 @@ internal class GeminiTemperatureBehavior(IOptionsMonitor<CellmAddInConfiguration
 
         // Scale temperature from [0;1] to [0;2]
         temperature = (temperature / DefaultMaxTemp) * GeminiMaxTemperature;
-        prompt.Options.Temperature = Math.Clamp(temperature, 0.0f, GeminiMaxTemperature);
+        prompt.Options.Temperature = Math.Clamp(temperature, DefaultMinTemp, GeminiMaxTemperature);
     }
 
-    // No-op
-    public void After(Prompt prompt) { }
+    public void After(Prompt prompt) 
+    {
+        if (prompt.Options.Temperature.HasValue)
+        {
+            var temperature = prompt.Options.Temperature.Value;
+
+            // Scale temperature from [0;2] back to [0;1]
+            temperature = (temperature / GeminiMaxTemperature) * DefaultMaxTemp;
+            prompt.Options.Temperature = Math.Clamp(temperature, DefaultMinTemp, DefaultMaxTemp);
+        }
+    }
 
     public UInt32 Order => 10;
 }
