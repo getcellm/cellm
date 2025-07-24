d1c6f3a2bddd2458fb183beef9a2d8db45539ddc
Kasper Marstal <kaspermarstal@gmail.com>
2025-07-24 21:20:13 +0200

    fix: Stabilize prompt prefix, granular timestamps kill kv-cache hit rate (#241)

--- src/Cellm/AddIn/CellmFunctions.cs
+++ src/Cellm/AddIn/CellmFunctions.cs
@@ -334,7 +334,7 @@ public static class CellmFunctions
                 .SetTemperature(arguments.Temperature)
                 .SetMaxOutputTokens(cellmAddInConfiguration.CurrentValue.MaxOutputTokens)
                 .SetOutputShape(arguments.OutputShape)
-                .AddSystemMessage(SystemMessages.SystemMessage(arguments.Provider, arguments.Model, DateTime.UtcNow))
+                .AddSystemMessage(SystemMessages.SystemMessage(arguments.Provider, arguments.Model, DateTime.Today))
                 .AddUserMessage(userMessage)
                 .Build();
 
